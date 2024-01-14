bb67504ae730cdefe75879c99ce5e0218af14bca
nokken65 <unknown65182@gmail.com>
2024-01-14 09:25:02 +0300

    refactor: remove ramda

--- src/app/providers/index.ts
+++ src/app/providers/index.ts
@@ -1,9 +1,9 @@
-import { pipe } from 'ramda'
+import { compose } from '@reduxjs/toolkit'
 
 import { type WithStoreConfig, withStore } from './withStore'
 
 type WithProvidersConfig = WithStoreConfig
 
-const withProviders = ({ store }: WithProvidersConfig) => pipe(withStore({ store }))
+const withProviders = ({ store }: WithProvidersConfig) => compose(withStore({ store }))
 
 export { type WithProvidersConfig, withProviders }
